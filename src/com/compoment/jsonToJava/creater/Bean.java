ababc0bfdd5767e5988062a2acb1dfb81c2faeda
db1600800 <db1600800@163.com>
2013-10-11 11:06:34 +0800

    jsonTojava ok
    gson json-lib

---
+++ src/com/compoment/jsonToJava/creater/Bean.java
@@ -0,0 +1,12 @@
+package com.compoment.jsonToJava.creater;
+
+import java.util.List;
+
+public class Bean{
+public List<PeopleBean> people;
+public class PeopleBean{
+public String firstName;
+public String lastName;
+public String email;
+}
+}
--- src/com/compoment/jsonToJava/creater/Pojo.java
+++ src/com/compoment/jsonToJava/creater/Pojo.java
@@ -8,34 +8,41 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections.map.ListOrderedMap;
 import org.apache.commons.io.FileUtils;
 
+
+
+import com.google.gson.Gson;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
-
-
-
+//http://www.cnblogs.com/hoojo/archive/2011/04/21/2023805.html
 public class Pojo {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Pojo pojo=new Pojo();
-		pojo.jsonFromFile();
+		Pojo pojo = new Pojo();
+		String jsonString= pojo.jsonFromFile();
+		//String jsonString=pojo.jsonFromUrl();
+		JSONObject jsonObject = JSONObject.fromObject(jsonString);
+		System.out.println("public class Bean{");
+		pojo.getPojo(jsonObject);
 
-
+		pojo.addJsonValueToBean(jsonObject,jsonString);
 	}
 
-
-   public void jsonFromUrl()
-   {
+	/**从url得到json数据*/
+	public String jsonFromUrl() {
 		try {
 			String urlPath = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=210af0ac7c5dad997a19f7667e5779d3&tags=Singapore&per_page=200&format=json&nojsoncallback=1";
 			URL url = new URL(urlPath);
@@ -53,32 +60,35 @@ public class Pojo {
 			}
 			reader.close();
 			connection.disconnect();
-			JSONObject jsonObject = JSONObject.fromObject(sb.toString()); // ����JSONObject����
-			System.out.println("public class Bean{");
-			getPojo(jsonObject);
+		return sb.toString();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-   }
-
-   public void jsonFromFile()
-   {
-	   String classDir = this.getClass().getResource("/").getPath();
-	   String jsonString;
-	try {
-		jsonString = FileUtils.readFileToString(new File(classDir+"com/compoment/jsonToJava/creater/sample.json"));
-		JSONObject jsonObject = JSONObject.fromObject(jsonString); // ����JSONObject����
-		System.out.println("public class Bean{");
-		getPojo(jsonObject);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		return null;
 	}
 
-   }
+	/**从文件得到json数据*/
+	public String jsonFromFile() {
+		String classDir = this.getClass().getResource("/").getPath();
+		String jsonString = null;
+		try {
+			jsonString = FileUtils.readFileToString(new File(classDir
+					+ "com/compoment/jsonToJava/creater/sample.json"));
+			return jsonString;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsonString;
 
-	public static void getPojo(JSONObject jsonObject) {
+	}
+
+
+  	int i=0;
+	 /**生成Bean  json->bean*/
+	public  void getPojo(JSONObject jsonObject) {
+
 		try {
 			Set set = jsonObject.keySet();
 			Iterator it = set.iterator();
@@ -89,8 +99,8 @@ public class Pojo {
 
 				if (obj instanceof JSONArray) {
 					// List<PhotoBean> photoBeanList;
-					System.out.println("List<" + firstCharUpperCase(key)
-							+ "Bean> " + key + "BeanList;");
+					System.out.println("public List<" + firstCharUpperCase(key)
+							+ "Bean> " + key + ";");
 					// public class PhotoBean {
 					System.out.println("public class "
 							+ firstCharUpperCase(key) + "Bean{");
@@ -100,8 +110,13 @@ public class Pojo {
 						getPojo(temp);
 					}
 				} else if (obj instanceof JSONObject) {
+
+						System.out.println("public "
+								+ firstCharUpperCase(key) + "Bean "+key+";");
+
 					System.out.println("public class "
 							+ firstCharUpperCase(key) + "Bean{");
+
 					JSONObject temp = (JSONObject) obj;
 					getPojo(temp);
 
@@ -118,6 +133,7 @@ public class Pojo {
 
 	}
 
+	/**首字母大写*/
 	public static String firstCharUpperCase(String s) {
 		if (s.length() > 0) {
 			String firstchar = String.valueOf(s.charAt(0)).toUpperCase();
@@ -128,6 +144,27 @@ public class Pojo {
 			return null;
 		}
 
+	}
+
+
+
+
+/**如何使用生成的Bean  即json->java*/
+	public void addJsonValueToBean(JSONObject jsonObject,String jsonString) {
+//		public class Bean{
+//			public List<PeopleBean> people;
+//			public class PeopleBean{
+//			public String firstName;
+//			public String lastName;
+//			public String email;
+//			}
+//			}
+		Gson gson = new Gson();
+		Bean bean = gson.fromJson(jsonString, Bean.class);
+		if(bean==null)
+		{
+        System.out.println(bean.toString());
+		}
 	}
 
 
