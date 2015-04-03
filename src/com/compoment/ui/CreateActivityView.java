1ace1c495f4c5af203c5fe9f2e6225232e889678
db1600800 <db1600800@163.com>
2015-04-03 11:25:51 +0800

    activity

--- src/com/compoment/ui/CreateActivityView.java
+++ src/com/compoment/ui/CreateActivityView.java
@@ -17,7 +17,6 @@ import org.xml.sax.SAXException;
 
 
 
-
 import com.compoment.util.FileUtil;
 import com.compoment.util.KeyValue;
 
@@ -30,7 +29,7 @@ public class CreateActivityView {
 	static String className = null;
 	String[] controls = { "Button", "TextView", "EditText", "ImageView",
 			"ExpandableListView", "ListView", "GridView", "Spinner",
-			"AutoCompleteTextView", "CheckBox" };
+			"AutoCompleteTextView", "CheckBox","android.support.v4.widget.DrawerLayout" };
 	Element root = null;
 	String m = "";
 
@@ -235,7 +234,13 @@ public class CreateActivityView {
 							+ firstCharToLowerAndJavaName(idToName[1])
 							+ "Adapter;\n";
 				}
-
+				else if (control.equals("android.support.v4.widget.DrawerLayout")) {
+					
+					m += control + " "
+							+ firstCharToLowerAndJavaName(idToName[1]) + ";\n";
+					m+="/**左抽屉布局*/\n";
+					m+="LinearLayout leftDrawerContain;\n";
+				}
 				else {
 
 					m += control + " "
@@ -251,55 +256,10 @@ public class CreateActivityView {
 		m += "  @Override\n";
 		m += "    public void onDestroy() {\n";
 		m += "        super.onDestroy();\n";
-		// m += "        clean();\n";
+		
 		m += " }\n";
-
-		// m += "	public void clean() {\n";
-		// m +=
-		// "InputMethodManager m = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);\n";
-		// m +=
-		// "m.hideSoftInputFromWindow(containView.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);\n";
-		// m += "		containView = null;\n";
-		// // m += "		containView = null;\n";
-		// for (String control : controls) {
-		// // control为Button TextView....
-		// NodeList buttonItems = root.getElementsByTagName(control);
-		// for (int i = 0; i < buttonItems.getLength(); i++) {
-		// Element personNode = (Element) buttonItems.item(i);
-		// String id = personNode.getAttribute("android:id");
-		// String text = personNode.getAttribute("android:text");
-		// String[] idToName = id.split("/");
-		// if (idToName == null || idToName.length < 2)
-		// continue;
-		//
-		// m += "//" + text + "\n";
-		// if (control.equals("ListView") || control.equals("GridView")) {
-		// m += firstCharToLowerAndJavaName(idToName[1]) + "=null;\n";
-		// m += "adapter.list.clear();\n";
-		// m += "adapter=null;\n";
-		// }
-		//
-		// else if (control.equals("AutoCompleteTextView")) {
-		// m += "	Cursor cursor="
-		// + firstCharToLowerAndJavaName(idToName[1])
-		// + "Adapter.getCursor();\n";
-		// m += "	if(cursor!=null)\n";
-		// m += "		cursor.close();\n";
-		// CreateSearchCursorAdapter createSearchCursorAdapter = new
-		// CreateSearchCursorAdapter();
-		// createSearchCursorAdapter
-		// .create(firstCharToUpperAndJavaName(idToName[1]));
-		// } else
-		//
-		// {
-		// m += firstCharToLowerAndJavaName(idToName[1]) + "=null;\n";
-		// }
-		// }
-		// }
-		//
-		// m += "}\n";
-
-	
+		
+		
 
 		m += "@Override\n";
 		m += "public void onCreate(Bundle savedInstanceState) {\n";
@@ -337,36 +297,6 @@ public class CreateActivityView {
 					m += firstCharToLowerAndJavaName(idToName[1])
 							+ ".setOnClickListener(new View.OnClickListener() {\n";
 					m += "						public void onClick(View v) {\n";
-
-					// m += "new  AsyncTask<String, Integer, Boolean>() {\n";
-					// m += "	@Override\n";
-					// m += "	protected void onPreExecute() {\n";
-					// m += "if (loading == null) {\n";
-					// m += "	loading = new LoadingProgressDialog();\n";
-					// m += "}\n";
-					// m +=
-					// "loading.showProgressDailg(\"提示\", \"加载中。。。\", context);\n";
-					// m += "	}\n";
-					//
-					// m += "	@Override\n";
-					// m +=
-					// "	protected Boolean doInBackground(String... params) {\n";
-					//
-					// List l = btnXmlnameTodbnameOrjavaname(idToName[1]);
-					// if (l.size() == 1) {
-					// m += l.get(0).toString();
-					// }
-					//
-					// m += "	}\n";
-					//
-					// m += "	@Override\n";
-					// m +=
-					// "	protected void onPostExecute(Boolean shoppingcarok) {\n";
-					// m += "//if(list==null || list.size()<1) return;\n";
-					// m += "	loading.cancleProgressDialog();\n";
-					// m += "	}\n";
-					//
-					// m += "}.execute(\"\");\n";
 
 					m += "}\n";
 					m += "					});\n";
@@ -450,8 +380,13 @@ public class CreateActivityView {
 					m += "		}\n";
 					m += "}\n";
 					m += ");\n";
+				} else if (control.equals("android.support.v4.widget.DrawerLayout")) {
+					
+					m += firstCharToLowerAndJavaName(idToName[1]) + "= ("
+							+ control + ") containView.findViewById(R.id."
+							+ idToName[1] + ");//将抽屉打开方法 mDrawerLayout.openDrawer(Gravity.LEFT);\n";
+					m+=" leftDrawerContain=(LinearLayout)containView.findViewById(R.id.leftDrawerContain);\n";
 				}
-
 				else {
 					m += firstCharToLowerAndJavaName(idToName[1]) + "= ("
 							+ control + ") containView.findViewById(R.id."
@@ -461,116 +396,11 @@ public class CreateActivityView {
 		}
 
 		m += "		}\n";
-		// m += "runAsyncTask();\n";
 		m += "request...();\n";
 		m += "return containView;\n";
 		m += "}\n";
 
-		// m += "public void runAsyncTask()\n";
-		// m += "{\n";
-		// m += "	GetNetInfoTask getNetInfoTask=new GetNetInfoTask();\n";
-		// m += "	getNetInfoTask.execute(\"\");\n";
-		// m += "}\n";
-
-		// m += "private class GetNetInfoTask extends\n";
-		// m += "			AsyncTask<String, Integer, List<" + className
-		// + "AdapterBean>> {\n";
-		// m += "List<" + className + "AdapterBean> list =new ArrayList();\n";
-		// m += "		@Override\n";
-		// m += "		protected void onPreExecute() {\n";
-		// m += "if (loading == null) {\n";
-		// m += "	loading = new LoadingProgressDialog();\n";
-		// m += "}\n";
-		// m += "loading.showProgressDailg(\"提示\", \"加载中。。。\", context);\n";
-		// for (String control : controls) {
-		// // control为Button TextView....
-		// NodeList buttonItems = root.getElementsByTagName(control);
-		// for (int i = 0; i < buttonItems.getLength(); i++) {
-		// Element personNode = (Element) buttonItems.item(i);
-		// String id = personNode.getAttribute("android:id");
-		// String text = personNode.getAttribute("android:text");
-		// String[] idToName = id.split("/");
-		// m += "//" + text + "\n";
-		// if (control.equals("ListView") || control.equals("GridView")) {
-		//
-		// m += "adapter.setList(list);\n";
-		// m += firstCharToLowerAndJavaName(idToName[1])
-		// + ".setAdapter(adapter);// 将数据适配器与Activity进行绑定\n";
-		// m += firstCharToLowerAndJavaName(idToName[1])
-		// + ".setOnScrollListener(" + className + ".this);\n";
-		//
-		// m += firstCharToLowerAndJavaName(idToName[1])
-		// + ".setOnItemClickListener(new OnItemClickListener(){\n";
-		// m += "		@Override\n";
-		// m +=
-		// "		public void onItemClick(AdapterView<?> arg0, View view,int position, long id) {\n";
-		// m += "	}});\n";
-		//
-		// }
-		// }
-		// }
-		//
-		// m += "		}\n";
-		//
-		// m += "@Override\n";
-		// m += "protected List<" + className
-		// + "AdapterBean> doInBackground(String... params) {\n";
-		// m += textviewXmlnameTodbnameOrjavaname();
-		// for (String control : controls) {
-		// // control为Button TextView....
-		// NodeList buttonItems = root.getElementsByTagName(control);
-		// for (int i = 0; i < buttonItems.getLength(); i++) {
-		// Element personNode = (Element) buttonItems.item(i);
-		// String id = personNode.getAttribute("android:id");
-		// String text = personNode.getAttribute("android:text");
-		// String[] idToName = id.split("/");
-		//
-		// if (control.equals("ListView") || control.equals("GridView")) {
-		// m += listXmlnameTodbnameOrjavaname(idToName[1]);
-		// }
-		// }
-		// }
-		//
-		// m += "			return null;\n";
-		// m += "}\n";
-		//
-		// m += "@Override\n";
-		// m += "protected void onProgressUpdate(Integer... values) {\n";
-		// m += "			// 更新进度\n";
-		// m += "		}\n";
-		//
-		// m += "		@Override\n";
-		// m += "	      protected void onPostExecute(List<" + className
-		// + "AdapterBean> list) {\n";
-		// m += "	loading.cancleProgressDialog();\n";
-		// m += "if(list==null || list.size()<1) return;\n";
-		// for (String control : controls) {
-		// // control为Button TextView....
-		// NodeList buttonItems = root.getElementsByTagName(control);
-		// for (int i = 0; i < buttonItems.getLength(); i++) {
-		// Element personNode = (Element) buttonItems.item(i);
-		// String id = personNode.getAttribute("android:id");
-		// String text = personNode.getAttribute("android:text");
-		// String[] idToName = id.split("/");
-		// if (idToName == null || idToName.length < 2)
-		// continue;
-		// m += "//" + text + "\n";
-		// if (control.equals("ListView") || control.equals("GridView")) {
-		// m += "adapter.notifyDataSetChanged();\n";
-		// } else if (control.equals("TextView")) {
-		// m += firstCharToLowerAndJavaName(idToName[1])
-		// + ".setText(\"\");\n";
-		// }
-		// }
-		// }
-		// m += "	      }\n";
-		//
-		// m += "		@Override\n";
-		// m += "protected void onCancelled() {\n";
-		// m += "			super.onCancelled();\n";
-		// m += "           list.clear();\n";
-		// m += "		}\n";
-		// m += "}\n";
+		
 
 	
 
