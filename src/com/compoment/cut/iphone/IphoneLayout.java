f8e5a3da93a2fb38301472beeb3b5e1ba1f8dea7
db1600800 <db1600800@163.com>
2015-06-04 17:26:55 +0800

    ios layout

--- src/com/compoment/cut/iphone/IphoneLayout.java
+++ src/com/compoment/cut/iphone/IphoneLayout.java
@@ -87,11 +87,6 @@ public class IphoneLayout {
 				+ "\" alpha=\"1\" colorSpace=\"calibratedRGB\"/>\n";
 
 		// m+="                        <constraints>\n";
-		// m+="                            <constraint firstItem=\"SyQ-o4-jyF\" firstAttribute=\"leading\" secondItem=\"kh9-bI-dsS\" secondAttribute=\"leadingMargin\" constant=\"20\" id=\"1hU-hb-12c\"/>\n";
-		// m+="                            <constraint firstAttribute=\"centerY\" secondItem=\"ktc-wb-5bz\" secondAttribute=\"centerY\" id=\"5gX-fc-Alk\"/>\n";
-		// m+="                            <constraint firstItem=\"2fi-mo-0CV\" firstAttribute=\"top\" secondItem=\"SyQ-o4-jyF\" secondAttribute=\"bottom\" constant=\"30\" id=\"pwU-eI-0fQ\"/>\n";
-		// m+="                            <constraint firstAttribute=\"trailingMargin\" secondItem=\"SyQ-o4-jyF\" secondAttribute=\"trailing\" constant=\"20\" id=\"q3b-qz-Qw5\"/>\n";
-		// m+="                            <constraint firstItem=\"ktc-wb-5bz\" firstAttribute=\"leading\" relation=\"lessThanOrEqual\" secondItem=\"kh9-bI-dsS\" secondAttribute=\"leadingMargin\" constant=\"20\" id=\"tqf-Ym-2sb\"/>\n";
 		// m+="                            <constraint firstAttribute=\"trailingMargin\" relation=\"lessThanOrEqual\" secondItem=\"ktc-wb-5bz\" secondAttribute=\"trailing\" constant=\"20\" id=\"vle-4u-4Vv\"/>\n";
 		// m+="                        </constraints>\n";
 		// m+="                        <variation key=\"default\">\n";
@@ -100,11 +95,6 @@ public class IphoneLayout {
 		// m+="                                <exclude reference=\"SyQ-o4-jyF\"/>\n";
 		// m+="                            </mask>\n";
 		// m+="                            <mask key=\"constraints\">\n";
-		// m+="                                <exclude reference=\"pwU-eI-0fQ\"/>\n";
-		// m+="                                <exclude reference=\"1hU-hb-12c\"/>\n";
-		// m+="                                <exclude reference=\"q3b-qz-Qw5\"/>\n";
-		// m+="                                <exclude reference=\"5gX-fc-Alk\"/>\n";
-		// m+="                                <exclude reference=\"tqf-Ym-2sb\"/>\n";
 		// m+="                                <exclude reference=\"vle-4u-4Vv\"/>\n";
 		// m+="                            </mask>\n";
 		// m+="                        </variation>\n";
@@ -115,12 +105,7 @@ public class IphoneLayout {
 		// m+="                            </mask>\n";
 		// m+="                            <mask key=\"constraints\">\n";
 		// m+="                                <include reference=\"pwU-eI-0fQ\"/>\n";
-		// m+="                                <include reference=\"1hU-hb-12c\"/>\n";
-		// m+="                                <include reference=\"q3b-qz-Qw5\"/>\n";
-		// m+="                                <include reference=\"5gX-fc-Alk\"/>\n";
-		// m+="                                <include reference=\"tqf-Ym-2sb\"/>\n";
-		// m+="                                <include reference=\"vle-4u-4Vv\"/>\n";
-		// m+="                            </mask>\n";
+		/// m+="                            </mask>\n";
 		// m+="                        </variation>\n";
 		m += constraint(maxBean);
 		m += "                    </view>\n";
@@ -156,17 +141,6 @@ public class IphoneLayout {
 							+ "\" alpha=\"1\" colorSpace=\"calibratedRGB\"/>\n";
 					// m+="                                <constraints>\n";
 					// m+="                                    <constraint firstItem=\"COT-hb-yaP\" firstAttribute=\"centerY\" secondItem=\"itq-au-h9W\" secondAttribute=\"centerY\" constant=\"-0.75\" id=\"4dZ-Um-rQ1\"/>\n";
-					// m+="                                    <constraint firstItem=\"0iP-qh-p3z\" firstAttribute=\"leading\" secondItem=\"IYI-bQ-iz6\" secondAttribute=\"trailing\" constant=\"10\" id=\"Fc6-1K-XdQ\"/>\n";
-					// m+="                                    <constraint firstItem=\"0iP-qh-p3z\" firstAttribute=\"centerY\" secondItem=\"IYI-bQ-iz6\" secondAttribute=\"centerY\" constant=\"1.75\" id=\"O6k-Fi-NBo\"/>\n";
-					// m+="                                    <constraint firstItem=\"0iP-qh-p3z\" firstAttribute=\"top\" secondItem=\"itq-au-h9W\" secondAttribute=\"bottom\" constant=\"19\" id=\"QJt-r8-NgS\"/>\n";
-					// m+="                                    <constraint firstItem=\"itq-au-h9W\" firstAttribute=\"top\" secondItem=\"ktc-wb-5bz\" secondAttribute=\"top\" constant=\"20\" id=\"RCq-lH-IOb\"/>\n";
-					// m+="                                    <constraint firstItem=\"COT-hb-yaP\" firstAttribute=\"leading\" secondItem=\"ktc-wb-5bz\" secondAttribute=\"leading\" constant=\"20\" id=\"Xnx-Hi-9Bk\"/>\n";
-					// m+="                                    <constraint firstItem=\"IYI-bQ-iz6\" firstAttribute=\"leading\" secondItem=\"ktc-wb-5bz\" secondAttribute=\"leading\" constant=\"20\" id=\"Y5f-kA-Y9q\"/>\n";
-					// m+="                                    <constraint firstAttribute=\"trailingMargin\" secondItem=\"itq-au-h9W\" secondAttribute=\"trailing\" constant=\"10\" id=\"ctI-ZM-EAO\"/>\n";
-					// m+="                                    <constraint firstItem=\"0iP-qh-p3z\" firstAttribute=\"centerY\" secondItem=\"IYI-bQ-iz6\" secondAttribute=\"centerY\" constant=\"1.75\" id=\"j41-yy-0bI\"/>\n";
-					// m+="                                    <constraint firstItem=\"itq-au-h9W\" firstAttribute=\"leading\" secondItem=\"COT-hb-yaP\" secondAttribute=\"trailing\" constant=\"10\" id=\"lmb-pJ-JUc\"/>\n";
-					// m+="                                    <constraint firstAttribute=\"bottomMargin\" secondItem=\"0iP-qh-p3z\" secondAttribute=\"bottom\" constant=\"20\" id=\"tPu-9L-QiK\"/>\n";
-					// m+="                                    <constraint firstAttribute=\"trailingMargin\" secondItem=\"0iP-qh-p3z\" secondAttribute=\"trailing\" constant=\"10\" id=\"wIu-1n-N1A\"/>\n";
 					// m+="                                </constraints>\n";
 					m += constraint(chirld);
 
@@ -193,24 +167,10 @@ public class IphoneLayout {
 
 					// m+="                                <variation key=\"default\">\n";
 					// m+="                                    <mask key=\"subviews\">\n";
-					// m+="                                        <exclude reference=\"IYI-bQ-iz6\"/>\n";
-					// m+="                                        <exclude reference=\"0iP-qh-p3z\"/>\n";
-					// m+="                                        <exclude reference=\"itq-au-h9W\"/>\n";
 					// m+="                                        <exclude reference=\"COT-hb-yaP\"/>\n";
 					// m+="                                    </mask>\n";
 					// m+="                                    <mask key=\"constraints\">\n";
 					// m+="                                        <exclude reference=\"Fc6-1K-XdQ\"/>\n";
-					// m+="                                        <exclude reference=\"O6k-Fi-NBo\"/>\n";
-					// m+="                                        <exclude reference=\"QJt-r8-NgS\"/>\n";
-					// m+="                                        <exclude reference=\"j41-yy-0bI\"/>\n";
-					// m+="                                        <exclude reference=\"tPu-9L-QiK\"/>\n";
-					// m+="                                        <exclude reference=\"wIu-1n-N1A\"/>\n";
-					// m+="                                        <exclude reference=\"4dZ-Um-rQ1\"/>\n";
-					// m+="                                        <exclude reference=\"Xnx-Hi-9Bk\"/>\n";
-					// m+="                                        <exclude reference=\"Y5f-kA-Y9q\"/>\n";
-					// m+="                                        <exclude reference=\"RCq-lH-IOb\"/>\n";
-					// m+="                                        <exclude reference=\"ctI-ZM-EAO\"/>\n";
-					// m+="                                        <exclude reference=\"lmb-pJ-JUc\"/>\n";
 					// m+="                                    </mask>\n";
 					// m+="                                </variation>\n";
 
@@ -223,18 +183,7 @@ public class IphoneLayout {
 					// m+="                                    </mask>\n";
 
 					// m+="                                    <mask key=\"constraints\">\n";
-					// m+="                                        <include reference=\"Fc6-1K-XdQ\"/>\n";
-					// m+="                                        <include reference=\"O6k-Fi-NBo\"/>\n";
-					// m+="                                        <include reference=\"QJt-r8-NgS\"/>\n";
-					// m+="                                        <include reference=\"j41-yy-0bI\"/>\n";
-					// m+="                                        <include reference=\"tPu-9L-QiK\"/>\n";
-					// m+="                                        <include reference=\"wIu-1n-N1A\"/>\n";
-					// m+="                                        <include reference=\"4dZ-Um-rQ1\"/>\n";
-					// m+="                                        <include reference=\"Xnx-Hi-9Bk\"/>\n";
-					// m+="                                        <include reference=\"Y5f-kA-Y9q\"/>\n";
-					// m+="                                        <include reference=\"RCq-lH-IOb\"/>\n";
-					// m+="                                        <include reference=\"ctI-ZM-EAO\"/>\n";
-					// m+="                                        <include reference=\"lmb-pJ-JUc\"/>\n";
+							// m+="                                        <include reference=\"lmb-pJ-JUc\"/>\n";
 					// m+="                                    </mask>\n";
 					// m+="                                </variation>\n";
 					m += "                            </view>\n";
@@ -264,6 +213,9 @@ public class IphoneLayout {
 					+ "\" height=\""
 					+ chirld.h
 					+ "\"/>\n";
+			
+			m+="<color key=\"backgroundColor\" red=\""+chirld.getR(chirld.bgRgb16)+"\" green=\""+chirld.getG(chirld.bgRgb16)+"\" blue=\""+chirld.getB(chirld.bgRgb16)+"\" alpha=\"1\" colorSpace=\"calibratedRGB\"/>\n";
+			
 			m += "                                        <fontDescription key=\"fontDescription\" type=\"system\" pointSize=\"17\"/>\n";
 			m += "                                        <color key=\"textColor\" red=\""
 					+ chirld.getR(chirld.rgb16)
@@ -443,38 +395,38 @@ public class IphoneLayout {
 					if ((chirld1.x + chirld1.w) < chirld2.x) {// 有人在你(chirld1)右边
 
 						if (right == true) {
-							if (rightvalue > chirld2.x
-									- (chirld1.x + chirld1.w)) {// 上一个距离远
-																// 换现在这个近的
-
-								String oldString = "<constraint firstItem=\""
-										+ chirld1.id
-										+ "\" firstAttribute=\"trailing\" secondItem=\""
-										+ chirld2.id
-										+ "\" secondAttribute=\"leading\" constant=\""
-										+ (rightvalue) + "\"";
-								leftvalue = chirld2.x - (chirld1.x + chirld1.w);
-								String newString = "<constraint firstItem=\""
-										+ chirld1.id
-										+ "\" firstAttribute=\"trailing\" secondItem=\""
-										+ chirld2.id
-										+ "\" secondAttribute=\"leading\" constant=\""
-										+ (rightvalue) + "\"";
-
-								m = m.replace(oldString, newString);
-
-							}
+//							if (rightvalue > chirld2.x
+//									- (chirld1.x + chirld1.w)) {// 上一个距离远
+//																// 换现在这个近的
+//
+//								String oldString = "<constraint firstItem=\""
+//										+ chirld1.id
+//										+ "\" firstAttribute=\"trailing\" secondItem=\""
+//										+ chirld2.id
+//										+ "\" secondAttribute=\"leading\" constant=\""
+//										+ (rightvalue) + "\"";
+//								leftvalue = chirld2.x - (chirld1.x + chirld1.w);
+//								String newString = "<constraint firstItem=\""
+//										+ chirld1.id
+//										+ "\" firstAttribute=\"trailing\" secondItem=\""
+//										+ chirld2.id
+//										+ "\" secondAttribute=\"leading\" constant=\""
+//										+ (rightvalue) + "\"";
+//
+//								m = m.replace(oldString, newString);
+//
+//							}
 						} else {
-							rightvalue = chirld1.x - (chirld2.x + chirld2.w);
-							String id=id();
-							m += "<constraint firstItem=\""
-									+ chirld1.id
-									+ "\" firstAttribute=\"trailing\" secondItem=\""
-									+ chirld2.id
-									+ "\" secondAttribute=\"leading\" constant=\""
-									+ (rightvalue) + "\" id=\"" + id
-									+ "\"/>\n";
-							n+=" <include reference=\""+id+"\"/>\n";
+//							rightvalue = chirld1.x - (chirld2.x + chirld2.w);
+//							String id=id();
+//							m += "<constraint firstItem=\""
+//									+ chirld1.id
+//									+ "\" firstAttribute=\"trailing\" secondItem=\""
+//									+ chirld2.id
+//									+ "\" secondAttribute=\"leading\" constant=\""
+//									+ (rightvalue) + "\" id=\"" + id
+//									+ "\"/>\n";
+//							n+=" <include reference=\""+id+"\"/>\n";
 							right = true;
 						}
 					}
@@ -519,39 +471,39 @@ public class IphoneLayout {
 					if ((chirld1.y + chirld1.h) < chirld2.y) {// 有人在你(chirld1)下边
 
 						if (bottom == true) {
-							if (bottomvalue > chirld2.y
-									- (chirld1.y + chirld1.h)) {// 上一个距离远
-																// 换现在这个近的
-
-								String oldString = "<constraint firstItem=\""
-										+ chirld1.id
-										+ "\" firstAttribute=\"bottom\" secondItem=\""
-										+ chirld2.id
-										+ "\" secondAttribute=\"top\" constant=\""
-										+ (bottomvalue) + "\"";
-								bottomvalue = chirld2.y
-										- (chirld1.y + chirld1.h);
-								String newString = "<constraint firstItem=\""
-										+ chirld1.id
-										+ "\" firstAttribute=\"bottom\" secondItem=\""
-										+ chirld2.id
-										+ "\" secondAttribute=\"top\" constant=\""
-										+ (bottomvalue) + "\"";
-
-								m = m.replace(oldString, newString);
-
-							}
+//							if (bottomvalue > chirld2.y
+//									- (chirld1.y + chirld1.h)) {// 上一个距离远
+//																// 换现在这个近的
+//
+//								String oldString = "<constraint firstItem=\""
+//										+ chirld1.id
+//										+ "\" firstAttribute=\"bottom\" secondItem=\""
+//										+ chirld2.id
+//										+ "\" secondAttribute=\"top\" constant=\""
+//										+ (bottomvalue) + "\"";
+//								bottomvalue = chirld2.y
+//										- (chirld1.y + chirld1.h);
+//								String newString = "<constraint firstItem=\""
+//										+ chirld1.id
+//										+ "\" firstAttribute=\"bottom\" secondItem=\""
+//										+ chirld2.id
+//										+ "\" secondAttribute=\"top\" constant=\""
+//										+ (bottomvalue) + "\"";
+//
+//								m = m.replace(oldString, newString);
+//
+//							}
 						} else {
-							bottomvalue = chirld2.y - (chirld1.y + chirld1.h);
-							String id=id();
-							m += "<constraint firstItem=\""
-									+ chirld1.id
-									+ "\" firstAttribute=\"bottom\" secondItem=\""
-									+ chirld2.id
-									+ "\" secondAttribute=\"top\" constant=\""
-									+ (bottomvalue) + "\" id=\"" + id
-									+ "\"/>\n";
-							n+=" <include reference=\""+id+"\"/>\n";
+//							bottomvalue = chirld2.y - (chirld1.y + chirld1.h);
+//							String id=id();
+//							m += "<constraint firstItem=\""
+//									+ chirld1.id
+//									+ "\" firstAttribute=\"bottom\" secondItem=\""
+//									+ chirld2.id
+//									+ "\" secondAttribute=\"top\" constant=\""
+//									+ (bottomvalue) + "\" id=\"" + id
+//									+ "\"/>\n";
+//							n+=" <include reference=\""+id+"\"/>\n";
 							bottom = true;
 						}
 					}
