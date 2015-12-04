407e38540c44fa702a157f178fe9c82eefd463b0
db1600800 <db1600800@163.com>
2015-12-04 17:07:56 +0800

    dfdf

--- src/com/compoment/jspcss/cssToJsp.java
+++ src/com/compoment/jspcss/cssToJsp.java
@@ -2,110 +2,253 @@ package com.compoment.jspcss;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-
 public class cssToJsp {
 
-	public void cssToJsp()
-	{
-		
-		
+	public cssToJsp() {
+
 		try {
-		String courseFile = null;
-		File directory = new File("");// 参数为空
-		courseFile = directory.getCanonicalPath();
-		
-		String pathCss = courseFile
-				+ "/src/com/compoment/jspcss/css.txt";
-		String pathJsp = courseFile
-				+ "/src/com/compoment/jspcss/jsp.txt";
 
-			
-		FileReader frCss = new FileReader(pathCss);
-		BufferedReader brCss = new BufferedReader(frCss);
-		
-		FileReader frJsp = new FileReader(pathJsp);
-		BufferedReader brJsp = new BufferedReader(frJsp);
+			String courseFile = null;
+			File directory = new File("");// 参数为空
+			courseFile = directory.getCanonicalPath();
 
-		String cssline;
-		String jspline;
-		while (brJsp.ready()) {
-			jspline = brJsp.readLine();
+			String pathCss = courseFile + "/src/com/compoment/jspcss/css.txt";
+			String pathJsp = courseFile + "/src/com/compoment/jspcss/jsp.txt";
 
-			if (jspline.length() >= 1 && jspline.contains("class=\"") ) {
+			FileReader frJsp = new FileReader(pathJsp);
+			BufferedReader brJsp = new BufferedReader(frJsp);
 
-				int p=jspline.indexOf("class=\"");
-				
-				int p2=jspline.substring(p+8).indexOf("\"")+p+8;
-				
-				String stypeName=(String) jspline.subSequence(p+8, p2);
-				
-				
-				while (brCss.ready()) {
-					cssline = brCss.readLine();
+			String jspline;
+
+			String newJsp = "";
+
+			while (brJsp.ready()) {
+				jspline = brJsp.readLine();
+
+				if (jspline.length() >= 1 && jspline.contains("class=\"")) {
+
+					int startP = jspline.indexOf("class=\"");
+					String temp = jspline.substring(startP + 7);
+					int endP = jspline.substring(startP + 7).indexOf("\"") + startP + 8;
+
+					String classDetail = jspline.subSequence(startP, endP).toString();
+					String styleName = (String) jspline.subSequence(startP + 7, endP - 1);
+					String styleNames[]=styleName.split(" ");
 					
-					if(cssline.contains("."+stypeName))
+					String tempStyle="";
+					for(String name :  styleNames)
 					{
 						
+						String style = "";
+						style = css1(pathCss, name);
 						
+						if (!style.equals("")) 
+						{
+							if(style.indexOf(";")!=-1)
+							{
+							 tempStyle+=style;
+							}else
+							{
+							 tempStyle+=style+";";
+							}
+						}
 					}
+					
+					
+
+						if (!tempStyle.equals("")) {
+							// System.out.println(style);
+							String chage = jspline.replace(classDetail, "style=\"" + tempStyle + "\"") + "\n";
+							// System.out.println(chage);
+							newJsp += chage;
+
+						} else {
+							newJsp += jspline + "\n";
+
+						}
+						
+					
+					
+					
+
+				} else {
+
+					newJsp += jspline + "\n";
 				}
-				
+
 			}
 
 		
+
+			brJsp.close();
+			frJsp.close();
+
+			 System.out.println(newJsp);
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		brCss.close();
-		frCss.close();
-
-	} catch (IOException e) {
-		e.printStackTrace();
 	}
-	}
-//
-//	public static void main(String[] args) {
-//		
-//		cssToJsp cssToJsp = new cssToJsp();
-//
-//		
-//		
-//			
-//	}
 
+	public static void main(String[] args) {
+
+		cssToJsp cssToJsp = new cssToJsp();
+
+	}
+
+
+
+	/**
+	 * .swiper { positionk };
+	 * */
+	public String css1(String pathCss, String keyName) {
+
+
+		FileReader frCss = null;
+		BufferedReader brCss=null;
+		try {
+
+			frCss = new FileReader(pathCss);
+
+			 brCss = new BufferedReader(frCss);
+			String cssLine = "";
+
+			while (brCss.ready()) {
+
+				cssLine += brCss.readLine() + "\n";
+
+				String txt = cssLine;// ".swiper { positionk }";
+
+				String re1 = "(\\.)"; // Any Single Character 1
+				String re2 = "(" + keyName + ")"; // Word 1
+				String re3 = "(\\s*)"; // White Space 1
+				String re4 = "(\\{)"; // Any Single Character 2
+				String re5 = "(\\s*)"; // White Space 2
+				String re6 = "(.*)"; // Variable Name 1
+				String re7 = "(\\s*)"; // White Space 3
+				String re8 = "(\\})"; // Any Single Character 3
+
+				Pattern p = Pattern.compile(re1 + re2 + re3 + re4 + re5 + re6 + re7 + re8,
+						Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+				Matcher m = p.matcher(txt);
+				if (m.find()) {
+
+					String c1 = m.group(1);
+					String word1 = m.group(2);
+					String ws1 = m.group(3);
+					String c2 = m.group(4);
+					String ws2 = m.group(5);
+					String var1 = m.group(6);
+					String ws3 = m.group(7);
+					String c3 = m.group(8);
+
+					brCss.close();
+					frCss.close();
+					
+					return var1;
+				}
+				
+			
+			}
+			
+			brCss.close();
+			frCss.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return "";
+	}
 	
 	
-	public static void main(String[] args)
-	  {
-		String txt=".swiper dfd{ position }";
+	
+	
+	
+	
+	/**
+	 *  a { positionk };
+	 * */
+	public String css2(String pathCss, String keyName) {
 
-	    String re1="(\\.)";	// Any Single Character 1
-	    String re2="(swiper)";	// Variable Name 1
-	    String re3="(\\s+)";	// White Space 1
-	    String re4="(\\{)";	// Any Single Character 2
-	    String re5=".*?";	// Non-greedy match on filler
-	    String re6="((?:[a-z][a-z]+))";	// Word 1
-	    String re7=".*?";	// Non-greedy match on filler
-	    String re8="(\\})";	// Any Single Character 3
 
-	    Pattern p = Pattern.compile(re1+re2+re3+re4+re5+re6+re7+re8,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-	    Matcher m = p.matcher(txt);
-	    if (m.find())
-	    {
-	        String c1=m.group(1);
-	        String var1=m.group(2);
-	        String ws1=m.group(3);
-	        String c2=m.group(4);
-	        String word1=m.group(5);
-	        String c3=m.group(6);
-	        System.out.print("("+c1.toString()+")"+"("+var1.toString()+")"+"("+ws1.toString()+")"+"("+c2.toString()+")"+"("+word1.toString()+")"+"("+c3.toString()+")"+"\n");
-	    }
-	  }
+		FileReader frCss = null;
+		BufferedReader brCss=null;
+		try {
+
+			frCss = new FileReader(pathCss);
+
+			 brCss = new BufferedReader(frCss);
+			String cssLine = "";
+
+			while (brCss.ready()) {
+
+				cssLine += brCss.readLine() + "\n";
+
+				String txt = cssLine;// ".swiper { positionk }";
+
+				String re1 = "(\\.)"; // Any Single Character 1
+				String re2 = "(" + keyName + ")"; // Word 1
+				String re3 = "(\\s*)"; // White Space 1
+				String re4 = "(\\{)"; // Any Single Character 2
+				String re5 = "(\\s*)"; // White Space 2
+				String re6 = "(.*)"; // Variable Name 1
+				String re7 = "(\\s*)"; // White Space 3
+				String re8 = "(\\})"; // Any Single Character 3
+
+				Pattern p = Pattern.compile(re1 + re2 + re3 + re4 + re5 + re6 + re7 + re8,
+						Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+				Matcher m = p.matcher(txt);
+				if (m.find()) {
+
+					String c1 = m.group(1);
+					String word1 = m.group(2);
+					String ws1 = m.group(3);
+					String c2 = m.group(4);
+					String ws2 = m.group(5);
+					String var1 = m.group(6);
+					String ws3 = m.group(7);
+					String c3 = m.group(8);
+//					System.out.println("(" + c1.toString() + ")" + "(" + word1.toString() + ")" + "(" + ws1.toString()
+//							+ ")" + "(" + c2.toString() + ")" + "(" + ws2.toString() + ")" + "(" + var1.toString() + ")"
+//							+ "(" + ws3.toString() + ")" + "(" + c3.toString() + ")" + "\n");
+					
+					brCss.close();
+					frCss.close();
+					
+					return var1;
+				}
+				
+			
+			}
+			
+			brCss.close();
+			frCss.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return "";
+	}
+	
+	
+
 	public static boolean isinteger(String value) {
 		try {
 			Integer.parseInt(value);
