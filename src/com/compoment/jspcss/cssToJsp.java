f68ab2d96ca5b22df468c3d946212ba4cab52f07
db1600800 <db1600800@163.com>
2016-02-25 17:45:02 +0800

    df

--- src/com/compoment/jspcss/cssToJsp.java
+++ src/com/compoment/jspcss/cssToJsp.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,13 +14,9 @@ public class cssToJsp {
 
 	public cssToJsp() {
 
-
 	}
 
-	
-	public void commonCssToJsp()
-	{
-		
+	public void commonCssToJsp() {
 
 		try {
 
@@ -41,9 +38,9 @@ public class cssToJsp {
 				jspline = brJsp.readLine();
 
 				if (jspline.length() >= 1 && jspline.contains("class=\"")) {
-					//img  a  ol  li  body  table  input  h1  h2  h3 h4  textarea  select  strong  span th  td  header  dd  dt
-				}
-				else if (jspline.length() >= 1 && jspline.contains("class=\"")) {
+					// img a ol li body table input h1 h2 h3 h4 textarea select
+					// strong span th td header dd dt
+				} else if (jspline.length() >= 1 && jspline.contains("class=\"")) {
 
 					int startP = jspline.indexOf("class=\"");
 					String temp = jspline.substring(startP + 7);
@@ -51,43 +48,33 @@ public class cssToJsp {
 
 					String classDetail = jspline.subSequence(startP, endP).toString();
 					String styleName = (String) jspline.subSequence(startP + 7, endP - 1);
-					String styleNames[]=styleName.split(" ");
-					
-					String tempStyle="";
-					for(String name :  styleNames)
-					{
-						
+					String styleNames[] = styleName.split(" ");
+
+					String tempStyle = "";
+					for (String name : styleNames) {
+
 						String style = "";
 						style = css1(pathCss, name);
-						
-						if (!style.equals("")) 
-						{
-							if(style.indexOf(";")!=-1)
-							{
-							 tempStyle+=style;
-							}else
-							{
-							 tempStyle+=style+";";
+
+						if (!style.equals("")) {
+							if (style.indexOf(";") != -1) {
+								tempStyle += style;
+							} else {
+								tempStyle += style + ";";
 							}
 						}
 					}
-					
-					
 
-						if (!tempStyle.equals("")) {
-							// System.out.println(style);
-							String chage = jspline.replace(classDetail, "style=\"" + tempStyle + "\"") + "\n";
-							// System.out.println(chage);
-							newJsp += chage;
+					if (!tempStyle.equals("")) {
+						// System.out.println(style);
+						String chage = jspline.replace(classDetail, "style=\"" + tempStyle + "\"") + "\n";
+						// System.out.println(chage);
+						newJsp += chage;
 
-						} else {
-							newJsp += jspline + "\n";
+					} else {
+						newJsp += jspline + "\n";
 
-						}
-						
-					
-					
-					
+					}
 
 				} else {
 
@@ -96,45 +83,40 @@ public class cssToJsp {
 
 			}
 
-		
-
 			brJsp.close();
 			frJsp.close();
 
-			 System.out.println(newJsp);
+			System.out.println(newJsp);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 
 		cssToJsp cssToJsp = new cssToJsp();
+
 		
-		cssToJsp.commonCssToJsp();
+		
+		
+		cssToJsp.cssToObject();
 	}
-
-
 
 	/**
 	 * .swiper { positionk };
-	 * */
+	 */
 	public String css1(String pathCss, String keyName) {
 
-
 		FileReader frCss = null;
-		BufferedReader brCss=null;
+		BufferedReader brCss = null;
 		try {
 
 			frCss = new FileReader(pathCss);
 
-			 brCss = new BufferedReader(frCss);
-	
+			brCss = new BufferedReader(frCss);
 
 			while (brCss.ready()) {
-
-				
 
 				String txt = brCss.readLine();// ".swiper { positionk }";
 
@@ -163,13 +145,12 @@ public class cssToJsp {
 
 					brCss.close();
 					frCss.close();
-					
+
 					return var1;
 				}
-				
-			
+
 			}
-			
+
 			brCss.close();
 			frCss.close();
 		} catch (FileNotFoundException e) {
@@ -179,18 +160,9 @@ public class cssToJsp {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return "";
 	}
-	
-	
-	
-	
-	
-
-	
-	
 
 	public static boolean isinteger(String value) {
 		try {
@@ -201,99 +173,178 @@ public class cssToJsp {
 		}
 	}
 
-	
-	
-	
-	
-	
-	
 	/**
 	 * .swiper { positionk };
-	 * */
-	
-	public String cssToObject(String pathCss, String keyName) {
+	 */
 
+	public String cssToObject() {
 
+		
+		String courseFile = null;
+		File directory = new File("");// 参数为空
+		try {
+			courseFile = directory.getCanonicalPath();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		String pathCss = courseFile + "/src/com/compoment/jspcss/css.txt";
+		
+		
 		FileReader frCss = null;
-		BufferedReader brCss=null;
+		BufferedReader brCss = null;
 		try {
 
 			frCss = new FileReader(pathCss);
 
-			 brCss = new BufferedReader(frCss);
-	
+			brCss = new BufferedReader(frCss);
+			List csss = new ArrayList();
 
 			while (brCss.ready()) {
-
-				
-
 				String txt = brCss.readLine();// ".swiper { positionk }";
 
-				//1.没有点 . (纯标签)
-				if(txt.indexOf(".")!=-1)
-				{
-					 String re1="(.*?)";	// Non-greedy match on filler
-					    String re2="(\\{)";	// Any Single Character 1
-					    String re3="(.*)";	// Variable Name 1
-					    String re4="(\\})";	// Any Single Character 2
+				// 1.没有点 . (纯标签)
+				if (txt.indexOf(".") == -1) {
+					String re1 = "(.*?)"; // Non-greedy match on filler
+					String re2 = "(\\{)"; // Any Single Character 1
+					String re3 = "(.*)"; // Variable Name 1
+					String re4 = "(\\})"; // Any Single Character 2
 
-					    Pattern p = Pattern.compile(re1+re2+re3+re4,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-					    Matcher m = p.matcher(txt);
-					    if (m.find())
-					    {
-					        String c1=m.group(1);
-					        String var1=m.group(2);
-					        String c2=m.group(3);
-					        String c3=m.group(4);
-					       
-					        
-					       String cleanTag[]= c1.split(",");
-					       
-					       for(int i=0;i<cleanTag.length;i++)
-					       {
-					    	   
-					       CssNode cssNode=new CssNode();
-					       cssNode.name=cleanTag[i];
-					       
-					       }
-					    }
-					
+					Pattern p = Pattern.compile(re1 + re2 + re3 + re4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+					Matcher m = p.matcher(txt);
+					if (m.find()) {
+						String c1 = m.group(1);
+						String var1 = m.group(2);
+						String c2 = m.group(3);
+						String c3 = m.group(4);
+
+						String cleanTag[] = c1.split(",");
+
+						for (int i = 0; i < cleanTag.length; i++) {
+
+							CssNode cssNode = new CssNode();
+							cssNode.name = cleanTag[i].trim();
+							String names[]=cssNode.name.split(" ");
+							String newname="";
+							if(names.length==1)
+							{
+								newname+=names[0];
+							}else
+							{
+							for(int j=0;j<names.length;j++)
+							{
+								
+								newname+=names[j]+"/";
+							}
+							}
+							cssNode.name=newname;
+							cssNode.cssString = c2;
+							csss.add(cssNode);
+
+						}
+					}
+
 				}
 				
 				
 				
-				String re1 = "(\\.)"; // Any Single Character 1
-				String re2 = "(" + keyName + ")"; // Word 1
-				String re3 = "(\\s*)"; // White Space 1
-				String re4 = "(\\{)"; // Any Single Character 2
-				String re5 = "(\\s*)"; // White Space 2
-				String re6 = "(.*)"; // Variable Name 1
-				String re7 = "(\\s*)"; // White Space 3
-				String re8 = "(\\})"; // Any Single Character 3
+				
+//			    String txt=".showmenu a .menuwrap { a }";
+				//
+//				    String re1="(\\.)";	// Any Single Character 1
+//				    String re2="(.*)";	// Variable Name 1 
+//				    String re3="(\\.)";	// White Space 1
+				//
+//				    Pattern p = Pattern.compile(re1+re2+re3,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+//				    Matcher m = p.matcher(txt);
+//				    while (m.find())
+//				    {
+//				        String c1=m.group(1);
+//				        String var1=m.group(2);
+//				        String ws1=m.group(3);
+//				     
+//				       
+//				        System.out.print("("+c1.toString()+")"+"("+var1.toString()+")"+"("+ws1.toString()+")\n");
+//				    }
+				//    
+				    
+				    
+				    
+				    String txt=".showmenu a .menuwrap-df df{ a }";
 
-				Pattern p = Pattern.compile(re1 + re2 + re3 + re4 + re5 + re6 + re7 + re8,
-						Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-				Matcher m = p.matcher(txt);
-				if (m.find()) {
+				    String re1="(\\.)";	// Any Single Character 1
+				    String re2="([\\w\\-]+\\s?\\w*)";	// Variable Name 1 
 
-					String c1 = m.group(1);
-					String word1 = m.group(2);
-					String ws1 = m.group(3);
-					String c2 = m.group(4);
-					String ws2 = m.group(5);
-					String var1 = m.group(6);
-					String ws3 = m.group(7);
-					String c3 = m.group(8);
+				    String re3="(\\{)";	// White Space 1
+				    
+				    Pattern p = Pattern.compile(re1+re2+re3,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+				    Matcher m = p.matcher(txt);
+				    while (m.find())
+				    {
+				        String c1=m.group(1);
+				        String var1=m.group(2);
+				        String ws1=m.group(3);
+				     
+				       
+				        System.out.print("("+c1.toString()+")"+"("+var1.toString()+")"+"("+ws1.toString()+")\n");
+				    }
+				    
+				    
+				
+				
+				
+				// 2.只有点 . (纯标签)
+				if (txt.indexOf(".") == -1) {
+					String re1 = "(.*?)"; // Non-greedy match on filler
+					String re2 = "(\\{)"; // Any Single Character 1
+					String re3 = "(.*)"; // Variable Name 1
+					String re4 = "(\\})"; // Any Single Character 2
 
-					brCss.close();
-					frCss.close();
-					
-					return var1;
+					Pattern p = Pattern.compile(re1 + re2 + re3 + re4, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+					Matcher m = p.matcher(txt);
+					if (m.find()) {
+						String c1 = m.group(1);
+						String var1 = m.group(2);
+						String c2 = m.group(3);
+						String c3 = m.group(4);
+
+						String cleanTag[] = c1.split(",");
+
+						for (int i = 0; i < cleanTag.length; i++) {
+
+							CssNode cssNode = new CssNode();
+							cssNode.name = cleanTag[i].trim();
+							String names[]=cssNode.name.split(" ");
+							String newname="";
+							if(names.length==1)
+							{
+								newname+=names[0];
+							}else
+							{
+							for(int j=0;j<names.length;j++)
+							{
+								
+								newname+=names[j]+"/";
+							}
+							}
+							cssNode.name=newname;
+							cssNode.cssString = c2;
+							csss.add(cssNode);
+
+						}
+					}
+
 				}
 				
-			
+				
+				
+				
+				
+				
+
 			}
-			
+
 			brCss.close();
 			frCss.close();
 		} catch (FileNotFoundException e) {
@@ -303,20 +354,15 @@ public class cssToJsp {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return "";
 	}
-	
-	public class CssNode
-	{
+
+	public class CssNode {
 		String name;
 		boolean hasChirld;
 		String cssString;
-		List<CssNode> nodes; 	
+		List<CssNode> nodes;
 	}
-	
-	
-	
-	
+
 }
