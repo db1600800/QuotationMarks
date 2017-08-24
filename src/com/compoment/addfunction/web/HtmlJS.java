56754bef386ec43be48278385a07b685d1b497d4
chinaposta1 <chinaposta1@163.com>
2017-08-24 17:18:36 +0800

    dsf

---
+++ src/com/compoment/addfunction/web/HtmlJS.java
@@ -0,0 +1,31 @@
+package com.compoment.addfunction.web;
+
+public class HtmlJS {
+
+	
+	public void p() {
+		
+		//选择器 1.选择器keyvalue(key中文value数字)数据写死  选择一个保存值到数据库
+		//      2.选择器keyvalue数据动态   选择一个保存值到数据库
+		//		3.选择器keyvalue数据动态    选择一个另外个选择器跟着变
+		//		4.选择器keyvalue数据写死    选择一个另外一个或多个控件隐藏
+		//      5.选择器keyvalue数据写死     选择一个另外一个图片控件图片切换 ，文字控件值变化，列表控件值变化，超链接控件变
+		
+		
+		//必填项左边标红星
+		
+		//按钮   1.保存
+		//       2.返回
+		//       3.清空图片，文件选择器
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
